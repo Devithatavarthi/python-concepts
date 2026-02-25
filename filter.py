c0048a96197820aa7be6d4e1da88d4015a0c7a34
Devithatavarthi <devithatavarthi22@gmail.com>
2026-02-25 15:32:28 +0530

    commit

--- filter.py
+++ filter.py
@@ -1,2 +1,5 @@
-# given a dictionary: d = {"apple": 100, "banana": 40, "cherry": 150} .
+# given a dictionary: d = {"apple": 100, "banana": 40, "cherry": 150}
 # Use filter() to keep only the keys whose values are greater than 50.
+d = {"apple": 100, "banana": 40, "cherry": 150}
+result=dict(filter(lambda x:x[1]>50,d.items()))
+print(result)
--- map.py
+++ map.py
@@ -1,5 +1,5 @@
  #Use map() with a lambda to add 5 to every element of the following nested list=[[1,2],[3,4],[5,6]]
 
 lst=[[1,2],[3,4],[5,6]]
-result=list(map(lambda sub:list(map(lambda x:x+5,sub)),lst))
+result=list(map(lambda inner:list(map(lambda x:x+5,inner)),lst))
 print(result)
